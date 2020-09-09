be8f261cdbc603903237b004a783d4552948b0b1
Adnan-110 <Adnan-110@users.noreply.github.com>
2020-09-09 21:47:21 +0530

    Create Question_1.js

---
+++ Day 4/Question_1.js
@@ -0,0 +1,17 @@
+function nextImage(){
+    const ele = document.getElementById("image");
+    ele.src = "https://cdna.artstation.com/p/assets/images/images/017/954/562/large/jackson-caspersz-brian-jackson-capt-w-mjolnir.jpg?1557975576";
+}
+
+
+function newImage(){
+    const ele = document.getElementById("image");
+    ele.src = "https://i.ytimg.com/vi/u1VCP3O8wG0/maxresdefault.jpg";
+}
+
+
+function backImage(){
+    const ele = document.getElementById("image");
+    ele.src = "https://www.pinkvilla.com/files/captain-america-butt.jpg";
+}
+
